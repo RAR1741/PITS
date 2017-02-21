0f87ed1b667b2f4d8d42e0ec256d599f1567982b
Jordan Miller <jmiller@springbuk.com>
2017-02-20 19:45:36 -0500

    Can actually pull down log files now

--- pits.rb
+++ pits.rb
@@ -1,43 +1,103 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'yaml'
 require 'slim'
 require 'git'
 require 'pry'
+require 'net/ssh'
+require 'net/scp'
 
-config = YAML.load(File.read('config.yaml'))
+# Main PITS class
+class PITS < Sinatra::Base
+  def initialize
+    super()
+    @config = YAML.load(File.read('config.yaml'))
+  end
 
-get '/' do
-  slim :index
+  get '/' do
+    slim :index
+  end
+
+  get '/logs' do
+    # Change to the log dir
+    # Dir.chdir('../RA17_LogFileArchive')
+
+    # add_test_file
+    # git_commit
+    pull_logs
+    # pp @error
+    # slim :error
+  end
+
+  get '/config' do
+    @config.inspect
+  end
+
+  def add_test_file
+    file_path =
+      File.join(
+        Dir.pwd,
+        @config['log_settings']['repo_path'], Time.now.to_i.to_s + '.txt'
+      )
+
+    file = File.open(file_path, 'a')
+    file.write('Some stuff and things...')
+    file.close
+  end
+
+  def git_commit
+    repo_path =
+      File.join(
+        Dir.pwd,
+        @config['log_settings']['repo_path']
+      )
+
+    # Setup the Git object
+    g = Git.open(repo_path, log: Logger.new(STDOUT))
+
+    # Add all files
+    g.add(all: true)
+
+    # Commit
+    g.commit(Time.now.strftime('Files added on %m/%d/%Y at %I:%M%p'))
+
+    # Push
+    g.push
+  end
+
+  def pull_logs
+    pp 'Doing a thing...'
+
+    Net::SSH.start(@config['robot']['ip'], @config['robot']['username'], :password => "") do |ssh|
+      file_string = ssh.exec!('ls -At *.csv')
+      files = file_string.split("\n")
+
+      # local_files = []
+
+      files.each do |file|
+        pp file
+        temp = ssh.scp.download(
+          file,
+          @config['log_settings']['local_path']
+        )
+
+        temp.wait
+
+        # local_files.push(temp)
+      end
+
+      # local_files.each(&:wait)
+    end
+    pp 'Done a thing...'
+  rescue SyntaxError, SocketError => error
+    pp 'Sockets was no...'
+    pp error
+  rescue StandardError => error
+    pp 'Something was no...'
+    pp error
+    # @error = 'Error: robot not found.   :('
+  end
 end
 
-get '/logs' do
-  # Change to the log dir
-  Dir.chdir('../RA17_LogFileArchive')
-
-  add_test_file
-  git_commit
-end
-
-get '/config' do
-  config.inspect
-end
-
-def add_test_file
-  file = File.open(Dir.pwd + '/' + Time.now.to_i.to_s + '.txt', 'a')
-  file.write('Some stuff and things...')
-  file.close
-end
-
-def git_commit
-  # Setup the Git object
-  g = Git.open(Dir.pwd, log: Logger.new(STDOUT))
-
-  # Add all files
-  g.add(:all=>true)
-
-  # Commit
-  g.commit(Time.now.strftime('Files added on %m/%d/%Y at %I:%M%p'))
-
-  # Push
-  g.push
-end
+PITS.run!
